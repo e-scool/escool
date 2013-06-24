31169240240a6b81505bec1a0a27af36c8c9ab6d
Victor Blasco <victor.bf@gmail.com>
2013-06-24 19:37:54 +0200

    Configure environments

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -65,4 +65,8 @@ Escool::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic") do |u, p|
+    [u, p] == ['e-scool', 'cool2013es']
+  end
 end
