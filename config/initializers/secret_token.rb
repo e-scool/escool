cb2c2c9a87ecb14b694acecee1434a8f1d4d613a
Victor Blasco <victor.bf@gmail.com>
2013-06-24 19:22:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Escool::Application.config.secret_token = '34cb9eb2574eaa7412755c6c3df025550a145f877f39b09144e5cb1c26ba9aa47760c3b99c31b0532b475822a725ad80b2935c3cbf587d706c27c090ffc244b8'
