542662b87b58f6e6ad62f85c5660e625d30a97c6
Victor Blasco <victor.bf@gmail.com>
2013-06-24 20:23:08 +0200

    ...Continue last commit

---
+++ app/models/teacher.rb
@@ -0,0 +1,19 @@
+class Teacher < User
+
+  # belongs_to :school
+  # belongs_to :current_classroom, :foreign_key => :current_classroom_id, :class_name => "Classroom"
+  # has_and_belongs_to_many :classrooms, :join_table => "classrooms_teachers"
+
+  # attr_accessible :classrooms
+
+
+  # def add_current_classroom
+  #   self.current_classroom_id = self.classrooms.first.id
+  #   self.save
+  # end
+
+  # def classroom_subjects(classroom)
+  #   classroom.subjects.where(:teacher_id => self.id).all
+  # end
+  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,44 @@
+class User < ActiveRecord::Base
+
+  validates :username, :uniqueness => true
+
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :lockable, :timeoutable, :omniauthable, :confirmable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :name, :username, :email, :password, :password_confirmation, :remember_me, :user_role
+  attr_accessible :login
+  attr_accessible :type
+
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+  ### This is the correct method you override with the code above
+  ### def self.find_for_database_authentication(warden_conditions)
+  ### end
+
+  def parent?
+    self.is_a?(Parent)
+  end
+
+  def teacher?
+    self.is_a?(Teacher)
+  end
+
+  def school_manager?
+    self.is_a?(SchoolManager)
+  end
+
+end
