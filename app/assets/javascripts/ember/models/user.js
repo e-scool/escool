3a05d188e5c1082073a0e7a55db5fac33c0c09c7
Victor Blasco <victor.bf@gmail.com>
2013-10-27 21:18:25 +0100

    (WIP) Adding current user actions

--- app/assets/javascripts/ember/models/user.js
+++ app/assets/javascripts/ember/models/user.js
@@ -1,27 +1,23 @@
 App.User = DS.Model.extend({
   // Database attrs
   name:     DS.attr('string'),
-  username: DS.attr('string')
+  username: DS.attr('string'),
+  type:     DS.attr('string'),
 
   // Relations
   // team:                       DS.belongsTo('team'),
   // friends:                    DS.hasMany('user',{async: true}),
 
   // Properties
-  // flagUrl: function(){
-  //   var countryName = this.get('countryName');
-  //   if (countryName){
-  //     return "/assets/flags/"+countryName.replace(' ','_')+".png"
-  //   }
-  // }.property('countryName'),
+  isParent: function(){
+    return this.get('type') == 'Parent';
+  }.property(),
 
-  // fullName: function(){
-  //   if (this.get('firstName') && this.get('lastName')){
-  //     return this.get('firstName') + ' ' + this.get('lastName');
-  //   }
-  // }.property('firstName','lastName'),
+  isTeacher: function(){
+    return this.get('type') == 'Teacher';
+  }.property(),
 
-  // isRival: function(){
-  //   return currentUserId && this.get('id') != currentUserId;
-  // }.property()
+  isSchoolManager: function(){
+    return this.get('type') == 'SchoolManager';
+  }.property()
 });
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,12 +3,18 @@ class Api::UsersController < Api::BaseController
   # GET /api/users?ids=[1,2,3]
   def index
     if params[:ids].kind_of?(Array)
-      @users = User.where(id: params[:ids]).becomes(User).decorate
+      @users = User.where(id: params[:ids]).becomes(User)
     end
   end
 
   def show
-    @user = User.find(params[:id]).becomes(User).decorate
+    user = User.find(params[:id])
+    type = user.type
+
+    if user == current_user
+      @user = user.becomes(User)
+      @user.type = type
+    end
   end
 
 end
