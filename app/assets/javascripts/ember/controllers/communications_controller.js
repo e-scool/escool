4486ee86055cb83b61946f67de18606ea5c606dc
Victor Blasco <victor.bf@gmail.com>
2013-10-29 21:20:02 +0100

    Trigger 'new' action from Materials and Communications Ember controllers

--- app/assets/javascripts/ember/controllers/communications_controller.js
+++ app/assets/javascripts/ember/controllers/communications_controller.js
@@ -1,4 +1,48 @@
 App.CommunicationsController = Ember.ArrayController.extend({
   needs: ['application'],
-  currentUser: Ember.computed.alias('controllers.application.currentUser')
+  currentUser: Ember.computed.alias('controllers.application.currentUser'),
+
+  actions: {
+    new: function () {
+      alert('new action!');
+    }
+
+    // buy: function(){
+    //   if(App.useFacebookPayments()){
+    //     this.send('buyFacebook');
+    //   }else{
+    //     if(Features.isActive('paymentwall')){
+    //       this.send('buyWall');
+    //     }else{
+    //       this.send('buyAdyen');
+    //     }
+    //   }
+    // },
+    // buyMobile: function(){
+    //   this.send('buyWall');
+    // },
+
+    // buyWall: function(){
+    //   this.transitionToRoute('ingotsPackages.show', this.get('content'));
+    // },
+
+    // buyFacebook: function(){
+    //   var self = this;
+    //   ga(function(tracker) {
+    //     var clientId = tracker.get('clientId');
+    //     window.fbBuyItem(self.get('id'), clientId);
+    //   });
+    // },
+
+    // buyAdyen: function(){
+    //   var id = this.get('id');
+    //   ga(function(tracker) {
+    //     $.get('/adyen/new_order', {
+    //       ingots_package_id: id
+    //     }, function (content) {
+    //       window.display_fancybox(content);
+    //     });
+    //   });
+    // }
+  }
 });
