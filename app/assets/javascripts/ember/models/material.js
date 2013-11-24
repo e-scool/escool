688e5c326131f1eeb2de8a6c6133fe8b20000a8d
Victor Blasco <victor.bf@gmail.com>
2013-11-24 17:16:25 +0100

    Add day and month to communications/materials templates using Moment.js

--- app/assets/javascripts/ember/models/material.js
+++ app/assets/javascripts/ember/models/material.js
@@ -1,5 +1,14 @@
 App.Material = DS.Model.extend({
   title: DS.attr('string'),
   body:  DS.attr('string'),
-  date:  DS.attr('date')
-}); 
+  date:  DS.attr('date'),
+
+  // Properties
+  day: function(){
+    return moment(new Date(this.get('date'))).format("DD");
+  }.property(),
+
+  month: function(){
+    return moment(new Date(this.get('date'))).format("MMMM");
+  }.property(),
+});
