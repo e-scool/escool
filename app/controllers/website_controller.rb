766d7028719cfde68520422bc9757897efbad012
Victor Blasco <victor.bf@gmail.com>
2013-12-15 21:57:48 +0100

    Improve code from WebsiteController

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,28 +1,28 @@
 class WebsiteController < ApplicationController
-
   respond_to :json, only: [:contact]
 
   before_filter :set_website_locale, only: [:index]
 
   def index
     @support_message = SupportMessage.new
-    render :layout => "website"
+
+    render layout: 'website'
   end
 
   def contact
-    @support_message = SupportMessage.new(params[:support_message])
+    message = SupportMessage.new(params[:support_message])
 
-    if @support_message.valid? && WebsiteMailer.contact_email(@support_message).deliver
-      sent = "sent"
-    else
-      sent = "no_sent"
-    end
+    action = if message.valid? && WebsiteMailer.contact_email(message).deliver
+               'sent'
+             else
+               'no_sent'
+             end
 
-    title    = t "website.contact_form_response_messages.#{sent}.title"
-    text     = t "website.contact_form_response_messages.#{sent}.text"
-    response = { title: title, text: text }
+    response = {
+      title: t("website.contact_form_response_messages.#{action}.title"),
+      text:  t("website.contact_form_response_messages.#{action}.text")
+    }
 
     respond_with response.to_json
   end
-
 end
