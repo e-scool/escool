c517da012f0c93bf52f29691b2ef577125a3b1fb
Victor Blasco <victor.bf@gmail.com>
2014-03-23 15:03:13 +0100

    Some CSS re-style

--- app/assets/javascripts/website.js
+++ app/assets/javascripts/website.js
@@ -23,14 +23,17 @@ $(document).ready(function() {
   // });
 
   // Main menu behaviour
-  $('ul#main-menu li a').click(function(){
-    prev = $(this).parent().parent().find("a.active").attr("rel");
-    next = $(this).attr("rel");
+  $('ul#main-menu li a').click(function() {
+    var prev = $(this).parent().parent().find("a.active").attr("rel");
+    var next = $(this).attr("rel");
+
     $(this).parent().parent().find("a.active").removeClass("active");
     $(this).addClass("active");
-    $("div.content-left#" + prev).stop(true, true).fadeOut(250, function(){
-     $("div.content-left#" + next).stop(true, true).fadeIn(250);
-   });
+
+    $("div.content-left." + prev).stop(true, true).fadeOut(250, function() {
+      $("div.content-left." + next).stop(true, true).fadeIn(250);
+    });
+
     return false;
   });
 
